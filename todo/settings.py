d69f55606e8e5e9523736f518a424fcfebe4883a
dishantsethi <dishantsethi14@gmail.com>
2020-11-24 16:21:34 +0530

    remove cors

--- todo/settings.py
+++ todo/settings.py
@@ -96,12 +96,11 @@ DATABASES = {
 }
 
 
-CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:8000',
-    'http://127.0.0.1:8000'
-]
+# CORS_ORIGIN_WHITELIST = [
+#     'http://localhost:8000',
+# ]
 # CORS_ALLOW_HEADERS = list(default_headers) + [
 #     'access-control-allow-origin',
 # ]
